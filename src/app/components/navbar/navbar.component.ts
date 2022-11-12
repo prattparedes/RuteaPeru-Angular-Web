8bba46b999baf3b89279762ae9af55169d1cdd67
Pratt <pratt.paredes@gmail.com>
2022-11-11 23:10:26 -0500

    home page mobile compatibility added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  cartQuantity=0;
+  public menuVisible = false;
+  barsIcon = faBars;
+  xIcon = faXmark;
+  cartQuantity = 0;
 
-  constructor(cartService:CartService) { 
+  constructor(cartService: CartService) {
     cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
-    })
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showMenu() {
+    if (this.menuVisible) this.closeMenu();
+    else this.menuVisible = true;
   }
 
+  closeMenu() {
+    this.menuVisible = false;
+  }
 }
