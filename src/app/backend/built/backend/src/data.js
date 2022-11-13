872957156cdd4e7e2b4200dfa1fc965bd1a04c43
Pratt <pratt.paredes@gmail.com>
2022-11-13 02:06:22 -0500

    made changes in package.json again

---
+++ src/app/backend/built/backend/src/data.js
@@ -0,0 +1,67 @@
+"use strict";
+exports.__esModule = true;
+exports.sample_tags = exports.sample_products = void 0;
+exports.sample_products = [
+    {
+        id: '1',
+        name: 'Fjällräven Gorra 1960 Logo Långtradarkeps',
+        brand: 'Fjällräven',
+        price: 120,
+        tags: ['gorras', 'ropa'],
+        color: 'black',
+        favorite: true,
+        stars: 4.0,
+        imageUrl: 'https://www.tradeinn.com/f/13801/138013361/fjallraven-gorra-1960-logo-langtradarkeps.jpg'
+    },
+    {
+        id: '2',
+        name: 'Columbia Chaleco Silver Ridge II',
+        brand: 'Columbia',
+        price: 180,
+        tags: ['chalecos', 'ropa'],
+        color: 'blue',
+        favorite: false,
+        stars: 3.0,
+        imageUrl: 'https://www.tradeinn.com/f/13789/137897877/columbia-chaleco-silver-ridge-ii.jpg'
+    },
+    {
+        id: '3',
+        name: 'TwoNav GPS Aventura 2',
+        brand: 'TwoNav',
+        price: 1200,
+        tags: ['gps', 'electrónica'],
+        color: 'metal',
+        favorite: true,
+        stars: 50,
+        imageUrl: 'https://www.tradeinn.com/f/13748/137488838/twonav-gps-aventura-2.jpg'
+    },
+    {
+        id: '4',
+        name: 'Botas Senderismo Terrex Swift R2 Mid Goretex',
+        brand: 'Adidas',
+        price: 460,
+        tags: ['botas', 'zapatos', 'ropa'],
+        color: 'blue',
+        favorite: true,
+        stars: 4.0,
+        imageUrl: 'https://www.tradeinn.com/f/13671/136718599/adidas-botas-senderismo-terrex-swift-r2-mid-goretex.jpg'
+    },
+    {
+        id: '5',
+        name: 'Brandit Pantalones Pure Vintage',
+        brand: 'Brandit',
+        price: 145.99,
+        tags: ['pantalones', 'ropa'],
+        color: 'green',
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'https://www.tradeinn.com/f/13802/138023065/brandit-pantalones-pure-vintage.jpg'
+    },
+];
+exports.sample_tags = [
+    { name: 'Todos', count: 5 },
+    { name: 'Ropa', count: 4 },
+    { name: 'Pantalones', count: 1 },
+    { name: 'Zapatos', count: 1 },
+    { name: 'Electrónica', count: 1 },
+];
---
+++ src/app/backend/built/models/tag.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Tag = void 0;
+var Tag = /** @class */ (function () {
+    function Tag() {
+    }
+    return Tag;
+}());
+exports.Tag = Tag;
